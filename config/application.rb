972909352446e9d2d7cf9fffcb5d3b52d1f038e7
Alfred Boateng <programmingwithalfred@gmail.com>
2022-08-01 10:25:40 +0000

    Setup complete
    Removed unused codes

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module Granite
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.1
+    # config.load_defaults 6.1
 
     # Configuration for the application, engines, and railties goes here.
     #
@@ -18,5 +18,8 @@ module Granite
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.generators do |g|
+     g.test_framework :test_unit, fixture: false
+    end
   end
 end
