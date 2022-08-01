d195e6aa4ca79c39fae430c5cba17b8a04973d61
Alfred Boateng <programmingwithalfred@gmail.com>
2022-08-01 16:13:22 +0000

    Added git hooks, eslint, prettier and rubocop

--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -1,7 +1,9 @@
-import React, { useEffect, useState } from 'react';
-import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
-import { setAuthHeaders } from 'apis/axios';
-import { initializeLogger } from 'common/logger';
+import React, { useEffect, useState } from "react";
+
+import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
+
+import { setAuthHeaders } from "apis/axios";
+import { initializeLogger } from "common/logger";
 
 const App = () => {
   const [loading, setLoading] = useState(true);
--- config/application.rb
+++ config/application.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require_relative "boot"
 
 require "rails/all"
@@ -19,7 +21,7 @@ module Granite
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.generators do |g|
-     g.test_framework :test_unit, fixture: false
+      g.test_framework :test_unit, fixture: false
     end
   end
 end
