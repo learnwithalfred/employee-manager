a4d9cbdd10ed15b45ccfe52ac6941fa219c04d69
Alfred Boateng <programmingwithalfred@gmail.com>
2022-08-02 05:20:38 +0000

    Added API based architecture

--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -20,7 +20,7 @@ const App = () => {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" render={() => <div>Home</div>} />
+        <Route exact path="/" render={() => <div> Home Page</div>} />
         <Route exact path="/about" render={() => <div>About</div>} />
       </Switch>
     </Router>
